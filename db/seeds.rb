db6250cb1655a67fb4b8a71f1320162151ff793a
Samuel Shobo <samuelshobo06@gmail.com>
2023-11-15 16:40:19 +0000

    added restuarant and review model, plus validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restuarants..."
+hakkasan = { name: "Hakkasan", category: "chinese", address: "Mayfair" }
+prezzo = { name: "Prezzo", category: "italian", address: "Lakeside" }
+wasabi = { name: "Wasabi", category: "japanese", address: "Embankment" }
+zedel = { name: "Zedel Brassiere", category: "french", address: "Bank" }
+cococure = { name: "Cococure", category: "belgian", address: "Algate East" }
+
+[hakkasan, prezzo, wasabi, zedel, cococure].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+puts "Finished!"
